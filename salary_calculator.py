aa1906395f2840bdca121dff5e0ffd839c62aa30
Chloe Chong <chloe.chong@hamilton-barnes.com>
2025-11-06 14:57:46 +0800

    test2

--- salary_calculator.py
+++ salary_calculator.py
@@ -40,7 +40,8 @@ st.image("hb_logo.png", width=250)
 
 # --- Salary data (role -> location -> (min, max)) ---
 salary_data = {
-    "--- Network Engineer ---"
+    "--- Network Engineer ---"}
+    
     "Network Engineer": {
         "London": (55000, 70000),
         "Manchester": (40000, 55000),
